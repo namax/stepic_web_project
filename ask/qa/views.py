95a360225b7fcbbc98e7685eee7aecd838a54197
Max <namax25@gmail.com>
2016-03-13 16:48:01 -0400

    Отображение данных v0.0.3 вывод вопроса и ответов

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,10 +1,7 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
-from qa.models import Question
-# Create your views here.
-
-
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage
+from django.shortcuts import render, get_object_or_404
+from qa.models import Question, Answer
+from django.http import HttpResponse, Http404
 
 
 def test(request, *args, **kwargs):
@@ -16,32 +13,52 @@ def not_found(request, *args, **kwargs):
 
 
 def question_details(request, question_id):
-    return HttpResponse(question_id)
+    question = get_object_or_404(Question, id=question_id)
+    answers = Answer.objects.filter(question=question)
+    return render(request, 'qa/question_detail.html', {
+        "question": question,
+        "answers": answers.all(),
+        "title": "Question detail"
+    })
 
 
 def popular_questions_list(request):
     limit = 10
-    questions = Question.objects.order_by("rating");
-    pageNumber = request.GET.get('page', 1);
+    questions = Question.objects.order_by("rating")
+    try:
+        page_number = request.GET.get('page', 1)
+    except ValueError:
+        raise Http404
     paginator = Paginator(questions, limit)
     paginator.baseurl = "/?page="
-    page = paginator.page(pageNumber)
+
+    try:
+        page = paginator.page(page_number)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
 
     return render(request, 'qa/questions_list.html', {
         "questions": page.object_list,
         "paginator": paginator,
         "page": page,
-        "title": "Last questions"
+        "title": "Popular questions"
     })
 
 
 def last_questions_list(request):
     limit = 10
-    questions = Question.objects.order_by("-id");
-    pageNumber = request.GET.get('page', 1);
+    questions = Question.objects.order_by("-id")
+    try:
+        page_number = request.GET.get('page', 1)
+    except ValueError:
+        raise Http404
+
     paginator = Paginator(questions, limit)
     paginator.baseurl = "/popular/?page="
-    page = paginator.page(pageNumber)
+    try:
+        page = paginator.page(page_number)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
 
     return render(request, 'qa/questions_list.html', {
         "questions": page.object_list,
